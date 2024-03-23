ee91eac33a8e1dc22f5b42e712de4f2ee5d27c7f
anirban <anirbanroy88@gmail.com>
2024-03-23 22:35:16 +0530

    Initial commit to create project

---
+++ src/main/java/com/personal/anirban/babyproducts/BabyProductsApplication.java
@@ -0,0 +1,13 @@
+package com.personal.anirban.babyproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabyProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BabyProductsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personal/anirban/babyproducts/BabyProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.anirban.babyproducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabyProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
